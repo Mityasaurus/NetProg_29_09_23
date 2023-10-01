4a4729663c8899f2087e3be66504bea53211793a
Mityasaurus <dmitriybovdun5@gmail.com>
2023-10-01 20:51:29 +0300

    Realation human - human completed

--- ClientConApp/Program.cs
+++ ClientConApp/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace ClientConApp
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientConApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter ip address");
+            IPAddress ip;
+            if(IPAddress.TryParse(Console.ReadLine(), out ip) == false)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+            Console.WriteLine("Enter port");
+            int port;
+            if (int.TryParse(Console.ReadLine(), out port) == false)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
+
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(ip, port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                socket.Connect(endPoint);
+                Console.WriteLine("Connection success");
+
+                bool job = true;
+
+                while(job)
+                {
+                    string message = Console.ReadLine();
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    await socket.SendAsync(data, SocketFlags.None);
+
+                    data = new byte[256];
+                    StringBuilder sb = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = await socket.ReceiveAsync(data, SocketFlags.None);
+                        sb.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                        Console.WriteLine($"{sb}");
+                        if(sb.ToString().ToLower() == "bye")
+                        {
+                            socket.Close();
+                            job = false;
+                            break;
+                        }
+                    } while (socket.Available > 0);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
---
+++ ServerApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int port = 8080;
+            string ip = "127.0.0.1";
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Bind(endPoint);
+                socket.Listen();
+
+                Console.WriteLine("Server started. Waiting for connections...");
+                Socket client = await socket.AcceptAsync();
+                Console.WriteLine("Client connected");
+
+                bool job = true;
+
+                while(job)
+                {
+                    byte[] data = new byte[256];
+                    StringBuilder sb = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = await client.ReceiveAsync(data, SocketFlags.None);
+                        sb.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                        Console.WriteLine($"{sb}");
+                        if(sb.ToString().ToLower() == "bye")
+                        {
+                            socket.Close();
+                            job = false;
+                        }
+                    } while (client.Available > 0);
+
+                    if(job == false)
+                    {
+                        break;
+                    }
+
+                    string message = Console.ReadLine();
+                    data = Encoding.UTF8.GetBytes(message);
+                    await client.SendAsync(data, SocketFlags.None);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
